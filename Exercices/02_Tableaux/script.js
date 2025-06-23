d7087f03d690c90532cbfd94d733cef408a3bcdd
Quentin Geerts <quentin.geerts@bstorm.be>
2025-06-23 16:05:11 +0200

    Énoncé exercice sur les tableaux

---
+++ Exercices/02_Tableaux/script.js
@@ -0,0 +1,8 @@
+/*
+
+  Créer un programme qui permet de demander à l'utilisateur combien de notes à rentrer.
+  Boucle sur le nombre de notes et les ajoute à un tableau.
+
+  Calcul la somme et la moyenne des notes. Ensuite les affiche.
+
+*/
