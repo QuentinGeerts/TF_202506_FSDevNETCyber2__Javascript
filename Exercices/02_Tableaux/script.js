6e9d291cfd6acbf2f3b7f01b295c8334c47b9b75
Quentin Geerts <quentin.geerts@bstorm.be>
2025-06-23 16:39:09 +0200

    Correction exercice tableau

--- Exercices/02_Tableaux/script.js
+++ Exercices/02_Tableaux/script.js
@@ -1,8 +1,22 @@
 /*
 
-  Créer un programme qui permet de demander à l'utilisateur combien de notes à rentrer.
+  Créer un programme qui permet de demander à l'utilisateur combien de notes il doit rentrer.
   Boucle sur le nombre de notes et les ajoute à un tableau.
 
   Calcul la somme et la moyenne des notes. Ensuite les affiche.
 
 */
+
+const notes = [];
+const NB_NOTES = parseInt(prompt("Combien de notes à rentrer :"));
+
+for (let i = 0; i < NB_NOTES; i++) {
+  notes.push(parseInt(prompt("Entrez la note n°" + (i + 1) + ": ")));
+}
+
+const somme = notes.reduce((acc, value) => acc + value);
+const moyenne = somme / NB_NOTES;
+
+console.log('notes :>> ', notes);
+console.log('somme :>> ', somme);
+console.log('moyenne :>> ', moyenne.toFixed(2));
